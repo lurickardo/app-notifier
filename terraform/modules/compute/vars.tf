625c011bf3f294420ffca8b647ce700722c8ee26
Lurickardo <luizr726@gmail.com>
2023-10-04 20:00:12 -0300

    Inserindo validação de ami

--- terraform/modules/compute/vars.tf
+++ terraform/modules/compute/vars.tf
@@ -18,82 +18,82 @@ variable "rds_dbname" {}
 # COMPUTE VARS DEFAULT VALUES
 
 variable "ec2_lt_name" {
-    type    = string
-    default = "ec2_lt_name"
+  type    = string
+  default = "ec2_lt_name"
 }
 
 variable "ec2_lt_ami" {
-    type    = string
-    default = "ami-02e136e904f3da870"
-    validation {
-        condition     = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
-        error_message = "The ami value must be a valid AMI id, starting with \"ami-\"."
-    }
+  type    = string
+  default = "ami-02e136e904f3da870"
+  validation {
+    condition     = length(var.ec2_lt_ami) > 4 && substr(var.ec2_lt_ami, 0, 4) == "ami-"
+    error_message = "The ami value must be a valid AMI id, starting with \"ami-\"."
+  }
 }
 
 variable "ec2_lt_instance_type" {
-    type    = string
-    default = "t2.micro"
+  type    = string
+  default = "t2.micro"
 }
 
 variable "ec2_lt_ssh_key_name" {
-    type    = string
-    default = "ec2_lt_ssh_key_name"
+  type    = string
+  default = "ec2_lt_ssh_key_name"
 }
 
 variable "ec2_lb_name" {
-    type    = string
-    default = "ec2-lb-name"
+  type    = string
+  default = "ec2-lb-name"
 }
 
 variable "ec2_lb_tg_name" {
-    type    = string
-    default = "ec2-lb-tg-name"
+  type    = string
+  default = "ec2-lb-tg-name"
 }
 
 variable "ec2_lb_tg_protocol" {
-    type    = string
-    default = "HTTP"
+  type    = string
+  default = "HTTP"
 }
 
 variable "ec2_lb_tg_port" {
-    type    = number
-    default = 80
+  type    = number
+  default = 80
 }
 
 variable "ec2_lb_listener_protocol" {
-    type    = string
-    default = "HTTP"
+  type    = string
+  default = "HTTP"
 }
 
 variable "ec2_lb_listener_port" {
-    type    = number
-    default = 80
+  type    = number
+  default = 80
 }
 
 variable "ec2_lb_listener_action_type" {
-    type    = string
-    default = "forward"
+  type    = string
+  default = "forward"
 }
 
 variable "ec2_asg_name" {
-    type    = string
-    default = "ec2-asg-name"
+  type    = string
+  default = "ec2-asg-name"
 }
 
 variable "ec2_asg_desired_capacity" {
-    type    = number
-    default = 8
+  type    = number
+  default = 8
 }
 
 variable "ec2_asg_min_size" {
-    type    = number
-    default = 4
+  type    = number
+  default = 4
 }
 
 variable "ec2_asg_max_size" {
-    type    = number
-    default = 16
+  type    = number
+  default = 16
 }
 
 # variable "access_key_id" {
